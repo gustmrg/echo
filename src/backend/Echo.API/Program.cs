2ab0b3cd4e41146e68953b1d55bb164eba5f5b6a
Gustavo Miranda <gustmrg@gmail.com>
2026-03-24 23:15:39 -0300

    feat(backend): add database setup

--- src/backend/Echo.API/Program.cs
+++ src/backend/Echo.API/Program.cs
@@ -1,9 +1,26 @@
 using Echo.API.Extensions;
+using Echo.Application.Interfaces;
+using Echo.Application.Services;
+using Echo.Application.Settings;
+using Echo.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApiDocumentation();
+
+builder.Services.AddOptions<JwtSettings>()
+    .BindConfiguration(JwtSettings.SectionName)
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSnakeCaseNamingConvention());
 
 var app = builder.Build();
 
